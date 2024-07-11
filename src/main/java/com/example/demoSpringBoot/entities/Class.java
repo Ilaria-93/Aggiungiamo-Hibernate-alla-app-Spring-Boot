25b7abe24190af71db6994a49365731780b9cab3
Ilaria Faleschini <ilaria.faleschini@gmail.com>
2024-07-11 10:52:51 +0200

    first commit

--- src/main/java/com/example/demoSpringBoot/entities/Class.java
+++ src/main/java/com/example/demoSpringBoot/entities/Class.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class Class {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "title", nullable = false)
--- src/main/java/com/example/demoSpringBoot/entities/Enrollment.java
+++ src/main/java/com/example/demoSpringBoot/entities/Enrollment.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class Enrollment {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Enrollment(Long id) {
